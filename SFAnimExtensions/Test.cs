7e58577236dee495732307e9867abc49f4f8f42e
dmitrii.kozlov <dmitrij.kozlov.h@gmail.com>
2020-05-05 19:12:54 +0300

    Initial commit

---
+++ SFAnimExtensions/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SoulsFormats;
+
+namespace SFAnimExtensions
+{
+    public class Test
+    {
+        public Test()
+        {
+            BND4.Read("test.bnd");
+        }
+    }
+}
