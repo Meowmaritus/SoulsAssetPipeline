3a0e6cce2e2e5cb9546e4ae4007f86a5829fd98d
Meowmaritus <Meowmaritus@gmail.com>
2020-10-02 16:24:29 -0500

    nightly misc

---
+++ SoulsAssetPipeline/SapEnums.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoulsAssetPipeline
+{
+    public enum SoulsGames
+    {
+        None,
+        DES,
+        DS1,
+        DS1R,
+        DS2,
+        DS2SOTFS,
+        DS3,
+        BB,
+        SDT,
+    }
+}
--- SoulsAssetPipeline/SapMath.cs
+++ SoulsAssetPipeline/SapMath.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Assimp;
+using SoulsFormats;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,6 +18,21 @@ namespace SoulsAssetPipeline
         public static float Lerp(float a, float b, float s)
         {
             return a + ((b - a) * s);
+        }
+
+        public static FLVER.VertexColor ToFlverVertexColor(this Color4D c)
+        {
+            return new FLVER.VertexColor(c.A, c.R, c.G, c.B);
+        }
+
+        public static System.Numerics.Vector3 ToNumerics(this Vector3D v)
+        {
+            return new System.Numerics.Vector3(v.X, v.Y, v.Z);
+        }
+
+        public static System.Numerics.Quaternion ToNumerics(this Quaternion q)
+        {
+            return new System.Numerics.Quaternion(q.X, q.Y, q.Z, q.W);
         }
     }
 }
